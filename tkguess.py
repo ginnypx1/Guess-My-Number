9e1187484fc5e01bc671d0b022d681ffa7ff0fba
ginnypx1 <ginny.pennekamp@gmail.com>
2015-07-31 14:51:53 -0700

    Tinter Guess My Number game
    Added Play Again & Quit options, Changed the display to include a
    strike count

--- tkguess.py
+++ tkguess.py
@@ -1,3 +1,4 @@
+from quitter import Quitter
 from tkinter import *
 import random
 
@@ -6,6 +7,7 @@ class GuessMyNumber(Frame):
 	"creates an app with a simple game to guess a number 1 to 10"
 	def __init__(self, parent=None):
 		Frame.__init__(self, parent)
+		# game variables
 		self.my_number = random.randint(1, 10)
 		self.count = 0
 
@@ -15,9 +17,13 @@ class GuessMyNumber(Frame):
 		lab.pack(side=TOP, fill=BOTH)
 		labl.pack(side=TOP, fill=BOTH)
 
+		# create results/hint window
+		hint = Label(self, width=40, height=2)
+		hint.pack(side=TOP, fill=BOTH)
+
 		# create an entry for for your guess
 		form = Frame(self)
-		form.pack(side=LEFT, fill=X)
+		form.pack(side=TOP, fill=BOTH)
 		ent_lab = Label(form, text="Your guess: ", width=10)
 		ent_lab.pack(side=LEFT)
 		var = IntVar()
@@ -25,35 +31,43 @@ class GuessMyNumber(Frame):
 		ent.pack(side=LEFT)	
 
 		# creates button to check the guess/ enter game loop
-		but = Button(self, text="Check", width=5, height=3, command=(lambda: self.turn(var.get())))
+		but = Button(form, text="Check", width=5, height=3, command=(lambda: self.turn(var.get(), hint)))
 		but.pack(side=RIGHT, fill=X, expand=YES)
+
+		# creates play again button
+		new_game = Button(self, text='Play again!', command=(lambda: self.new_game(hint)))
+		new_game.pack(side=TOP, fill=BOTH)
+
+		# creates quit button
+		quit = Quitter(self)
+		quit.pack(side=TOP, fill=BOTH)
 
 	def counter(self):
 		"keeps track of the number of guesses"
 		self.count += 1
 		return self.count
 
-	def turn(self, num):	
+	def turn(self, num, tag):	
 		"gameloop to guess a number 1 to 10 in 3 tries"
 		# count the guesses
 		total = self.counter() 
 		# if num == my_number:
 		if num == self.my_number:
-			# create a window for win
-			pop_win = Toplevel()
-			labl = Label(pop_win, text='You win! :)', width=20, height=10)
-			labl.pack(expand=YES, fill=BOTH)
+			# display win state
+			tag.config(text='You win! :)')
 		elif total == 3:
-			pop_loss = Toplevel()
-			llbl = Label(pop_loss, text='You lose! :(', width=20, height=10)
-			llbl.pack(expand=YES, fill=BOTH)
+			tag.config(text='You lose! :(')
 		else:
 			if num > self.my_number:
-				less = Label(self, text='Guess lower!', width=20)
-				less.pack(side=LEFT)
+				tag.config(text='Strikes: {}  Hint: guess lower...'.format(total))
 			else:
-				more = Label(self, text='Guess higher!', width=20)
-				more.pack(side=LEFT)
+				tag.config(text='Strikes: {}  Hint: guess higher...'.format(total))
+
+	def new_game(self, tag):
+		self.my_number = random.randint(1, 10)
+		self.count = 0
+		tag.config(text="I'm thinking of a new number.")
+
 
 if __name__ == '__main__':
 	root = Tk()
